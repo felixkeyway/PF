7d8e84318cfc3de4a167d04f25fd7bb5edb27089
Felix Jacobi <felix.jacobi@alice-dsl.net>
2012-07-07 23:25:54 +0300

    Fix problem with whitespaces

--- upload/KPFSetup/install/files/lib/system/exception/SystemException.class.php
+++ upload/KPFSetup/install/files/lib/system/exception/SystemException.class.php
@@ -3,14 +3,14 @@ namespace kpf\system\exception;
 
 /**
  * A SystemException will be thrown when an unexpected error occurs.
- * @author      Felix Jacobi
- * @license     GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl.html>
- * @package     com.keyway-xelonis.kpf
- * @subpackage  system.exception
- * @category    Portal Framework
+ * @author            Felix Jacobi
+ * @license           GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl.html>
+ * @package           com.keyway-xelonis.kpf
+ * @subpackage        system.exception
+ * @category          Portal Framework
  */
 
 class SystemException {
-  
+    
 }
 ?>
