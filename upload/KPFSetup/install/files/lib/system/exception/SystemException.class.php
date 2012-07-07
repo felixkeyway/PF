6c8d1ae32752b4bf0ad4c06ecfa9d33321feba84
Felix Jacobi <felix.jacobi@alice-dsl.net>
2012-07-07 18:08:32 +0300

    Added skeletal structure of the file.

---
+++ upload/KPFSetup/install/files/lib/system/exception/SystemException.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace kpf\system\exception;
+
+/**
+ * A SystemException will be thrown when an unexpected error occurs.
+ * @author      Felix Jacobi
+ * @license     GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl.html>
+ * @package     com.keyway-xelonis.kpf
+ * @subpackage  system.exception
+ * @category    Portal Framework
+ */
+
+class SystemException {
+  
+}
+?>
