94f304d00584dd655c38b925b6e51277c909ff26
Felix Jacobi <felix.jacobi@alice-dsl.net>
2012-07-07 11:34:49 +0200

    Added config.inc.php and global.php

---
+++ upload/KPFSetup/install/files/config.inc.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * This file contain all basic configuration of the portal framework.
+ */
+
+// make the path unix compatible
+$directoryReplace = str_replace('\\', '/', dirname(__file__));
+
+// define absolute portal framework directory
+define('KPF_DIR', $directoryReplace);
+
+// remove the document root
+$webdirectoryReplace = str_replace($_SERVER['DOCUMENT_ROOT'], '', KPF_DIR);
+
+// define the public web directory of the portal framework
+define('KPF_WEB_DIR', $webdirectoryReplace);
+
+// define the database host
+define('DB_HOST', 'localhost');
+
+// define the database name
+define('DB_NAME', 'kpf');
+
+// define the engine of the database
+define('DB_ENGINE', 'MySQL');
+
+// define the user of the database
+define('DB_USER', 'root');
+
+// define the password of the database user
+define('DB_PASSWORD', 'stern2');
+
+// define the port of the database server
+define('DB_PORT', 3306);
+
+// define the number of the installation
+define('DB_N', 1);
+
+// define the domain of the portal framework.
+define('KPF_DOMAIN', 'localhost');
+?>
---
+++ upload/KPFSetup/install/files/global.php
@@ -0,0 +1,18 @@
+<?php
+// kpf imports
+require_once './config.inc.php';
+
+// import kpf class file
+require_once KPF_DIR . 'lib/system/KPF.class.php';
+
+/**
+ * @author          Felix Jacobi <jacobi@keyway-xelonis.com>
+ * @copyright       2011-2012 Keyway-Xelonis GmbH
+ * @license         GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl.html>
+ * @package         com.keyway-xelonis.kpf
+ * @category        Portal Framework 
+ */
+
+// initiate kpf core
+$kpf = kpf\system\KPF::getInstance();
+?>
