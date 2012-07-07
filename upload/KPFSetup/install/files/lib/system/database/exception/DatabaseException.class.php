bd8318f79358d402be6f777b15752a4ce8fcd6d3
Felix Jacobi <felix.jacobi@alice-dsl.net>
2012-07-07 12:31:55 +0300

    Added cxopyright

--- upload/KPFSetup/install/files/lib/system/database/exception/DatabaseException.class.php
+++ upload/KPFSetup/install/files/lib/system/database/exception/DatabaseException.class.php
@@ -7,6 +7,7 @@ use kpf\system\execption\SystemException;
  * This class is a child of the class SystemException. It shows errors from the database.
  * 
  * @author          Felix Jacobi <jacobi@keyway-xelonis.com>
+ * @copyright       2011-2012 Keyway-Xelonis
  * @license         GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl.html>
  * @package         com.keyway-xelonis.kpf
  * @subpackage      system.database.exception
