c12730e36e0ef463a87572601d109c13a6826814
Felix Jacobi <felix.jacobi@alice-dsl.net>
2012-07-07 12:14:36 +0300

    Update master

--- upload/KPFSetup/install/files/lib/util/StringUtil.class.php
+++ upload/KPFSetup/install/files/lib/util/StringUtil.class.php
@@ -4,8 +4,8 @@ namespace kpf\util;
 /**
  * This class provides some util methods for string handling.
  * 
- * @author          Felix Jacobi <jacobi@keyway-xelonis.com>
- * @copyright       2011-2012 Keyway-Xelonis GmbH
+ * @author          Felix Jacobi
+ * @copyright       2011-2012 Keyway-Xelonis
  * @license         GNU Lesser General Public License <http://www.gnu.org/licenses/lgpl.html>
  * @package         com.keyway-xelonis.kpf
  * @subpackage      util
